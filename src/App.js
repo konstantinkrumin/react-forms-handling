e1e6a72eda150ae3957175bf3e1a2550edcbd1ce
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-10-20 02:40:10 +0300

    Добавил fetch request для получения данных с postman mock api

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './styles/main.scss';
 
 import RequestsList from './components/request-components/RequestsList'
 import Form from "./components/Form"
-import useFetch from "./helpers/fetchData"
 
 function App() {
-  // const DATA_LINK = 'https://027132dc-7417-472c-bcdd-4b9245888c75.mock.pstmn.io';
-  // const [data, loading] = useFetch(DATA_LINK);
+  const [data, setData] = useState()
+
+  const DATA_LINK = 'https://d76afe3b-6e74-4da7-b7ac-fff456f1bb50.mock.pstmn.io/data';
 
   useEffect(() => {
-    fetch('https://027132dc-7417-472c-bcdd-4b9245888c75.mock.pstmn.io/demo')
+    fetch(DATA_LINK)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(json => setData(json))
   }, [])
 
   return (
     <div className="main-window">
+      {console.log(data)}
       <h1>Hello World</h1>
       <RequestsList />
       <Form />
