c65c346739a01c70e2e393eaa8f9f64d1ce8ef0d
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-10-20 02:14:18 +0300

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/main.scss';
 
 import RequestsList from './components/request-components/RequestsList'
 import Form from "./components/Form"
-import fetchData from "./helpers/fetchData"
+import useFetch from "./helpers/fetchData"
 
 function App() {
+  // const DATA_LINK = 'https://027132dc-7417-472c-bcdd-4b9245888c75.mock.pstmn.io';
+  // const [data, loading] = useFetch(DATA_LINK);
+
+  useEffect(() => {
+    fetch('https://027132dc-7417-472c-bcdd-4b9245888c75.mock.pstmn.io/demo')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <div className="main-window">
       <h1>Hello World</h1>
