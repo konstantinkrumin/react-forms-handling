42ca90891ea47760ca9bc685878941ec40ac2bd1
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-10-20 21:00:00 +0300

    Updates

---
+++ precache-manifest.1cc281d7250303e61c138b12393b1448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ea0a19edc481a482b5091d28987e3e",
+    "url": "/react-forms-handling/index.html"
+  },
+  {
+    "revision": "cf8cd6572109e6a68652",
+    "url": "/react-forms-handling/static/css/main.19bcabf3.chunk.css"
+  },
+  {
+    "revision": "a9697d27f4074b9b647c",
+    "url": "/react-forms-handling/static/js/2.b4299749.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/react-forms-handling/static/js/2.b4299749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8cd6572109e6a68652",
+    "url": "/react-forms-handling/static/js/main.b74a73a7.chunk.js"
+  },
+  {
+    "revision": "e5a002e27036742c2ac4",
+    "url": "/react-forms-handling/static/js/runtime-main.95f814f8.js"
+  }
+]);
