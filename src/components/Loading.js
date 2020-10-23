3edbbea53d1a8fda800de83eb782858955f4be45
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-10-24 00:21:04 +0300

    Добавил спиннер в загрузочный компонет, установил фиксированную ширину на элементы формы и поменял в media query минимальную ширину грида и макс ширину media query

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className="loading-window">
-            {/* <h1 className="loading-window__message">Загрузка...</h1> */}
-            <div className="loading-window__message" />
+            <div className="loading-window__loader"/>
         </div>
     )
 }
